d88b4adbb90e45195e71dfccb22432d5770bcb1d
MalinovRoman <romanmalinov@yandex.ru>
2021-05-23 23:54:26 +0300

    решил 1 задачу Занятие 1: Объекты и переделал 2 задачу в Занятии 1

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -1,2 +1,24 @@
+function print(text) {
+  console.log(text);
+}
 
+function isValid(name) {
+  if (name.length >= 4 && name.indexOf(' ') == -1) {
+     return true;
+   } else {
+     return false;
+   }
+}
+
+function sayHello() {
+  let userName = prompt('Введите ваше имя');
+
+  if (isValid(userName)) {
+    print(`Welcome back, ${userName}!`);
+  } else {
+    print('Некорректное имя');
+  }
+}
+
+sayHello();
 
